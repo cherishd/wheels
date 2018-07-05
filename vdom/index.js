032c284861564be97546b9bc7875987a1d758cb6
cherishd <cherishdo@outlook.com>
2018-07-05 09:52:40 +0800

    [vdom] final test

--- vdom/index.js
+++ vdom/index.js
@@ -1,1 +1,16 @@
-console.log(1)
+import Element from './src/element'
+import diff from './src/diff'
+import patch from './src/patch'
+
+const el3 = new Element('div', { class: 'el3' }, ['el3'], 'el3')
+const el4 = new Element('span', { class: 'el4' }, ['el4'], 'el4')
+
+const el1 = new Element('div', { class: 'el1' }, [el3])
+const el2 = new Element('div', { class: 'el2' }, [el4])
+
+const root = el1.render()
+const patches = diff(el1, el2)
+
+setTimeout(() => {
+  patch(root, patches)
+}, 1000)
